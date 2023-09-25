b2f9d069f7a66d78d847173935c5dd4e0e06b903
atharvaghodekar <atharvaghodekar2121@gmail.com>
2023-09-25 16:39:19 +0530

    Assignment modified on 25/09/2023

--- Assignment_02TFIDF.py
+++ Assignment_02TFIDF.py
@@ -10,9 +10,9 @@ from gensim.utils import simple_preprocess
 from gensim import corpora
 
 
-text = ["The food is excellent but the service can be better",
-        "The food is always delicious and loved the service",
-        "The food was mediocre and the service was terriblejhk,."]
+text = ["My name is Atharva Ghodekar.", 
+   "I am from Sanjivani College of Engineering.", 
+   "Atharva is from Ahmednagar. "]
 
 g_dict = corpora.Dictionary([simple_preprocess(line) for line in text])
 g_bow = [g_dict.doc2bow(simple_preprocess(line)) for line in text]
@@ -26,3 +26,9 @@ g_tfidf = models.TfidfModel(g_bow, smartirs='ntc')
 print("TF-IDF Vector:")
 for item in g_tfidf[g_bow]:
     print([[g_dict[id], np.around(freq, decimals=2)] for id, freq in item])
+    
+#Output :
+#Dictionary : 
+#[['atharva', 1], ['ghodekar', 1], ['is', 1], ['my', 1], ['name', 1]]
+#[['am', 1], ['college', 1], ['engineering', 1], ['from', 1], ['of', 1], ['sanjivani', 1]]
+#[['atharva', 1], ['is', 1], ['from', 1], ['ahmednagar', 1]]
